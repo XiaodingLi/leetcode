b539536173fb374e8243946086d7577f47a5a027
XiaodingLi <lixiaoding07@gmail.com>
2015-03-20 15:48:20 -0400

    First O(n)  234ns
    1.1

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,32 @@
+package Tree;
+
+public class BalancedBinaryTree {
+	/**
+	 * Definition for binary tree
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(int x) { val = x; }
+	 * }
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	public boolean isBalanced(TreeNode root){
+		if(root== null)
+			return true;
+		if(Math.abs(depth(root.left)-depth(root.right))>1)
+			return false;
+		 return isBalanced(root.left) && isBalanced(root.right);
+	}
+	
+	private int depth(TreeNode root){
+		if(root==null)
+			return 0;
+		return 1 + Math.max(depth(root.left), depth(root.right));
+	}
+}
+  
